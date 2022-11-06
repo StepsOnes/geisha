aa3c297de640cba2086129a30a39b0e100b0455e
StepsOnes <stepan663663@gmail.com>
2022-11-06 22:31:03 +0300

    mobilePatch1.2(main-article)

--- js/owl-carousel.js
+++ js/owl-carousel.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         margin: 30,
         autoWidth: true,
         items: 4,
-        nav: true,
+        nav: false,
         navText: [
             '<button id="master-classes__btn-pred" class="master-classes__btn master-classes__btn--blue"></button>',
             '<button id="master-classes__btn-next" class="master-classes__btn master-classes__btn--blue"></button>'
@@ -43,7 +43,7 @@ $(document).ready(function() {
         responsive: {
             500: {
                 items: 1,
-                nav: false,
+                nav: true,
             }
         }
 
@@ -94,22 +94,23 @@ $(document).ready(function() {
     });
 
 
-    $(function () {
-        $(window).on('resize', function () {
-            var owlCarousel = $('.reviews-slaider-mobile').data('owlCarousel');
-            if ($(window).width() <= 767) {
-                !owlCarousel && $('.reviews-slaider-mobile').owlCarousel({
-                    dots: false,
-                    margin: 16,
-                    autoWidth: true,
-                    items: 0,
-                });
-            } else {
-                owlCarousel && owlCarousel.destroy();
-            }
-        }).trigger('resize');
-    })
+
 });
 
+$(function () {
+    $(window).on('resize', function () {
+        var owlCarousel = $('.reviews-slaider-mobile').data('owlCarousel');
+        if ($(window).width() <= 767) {
+            !owlCarousel && $('.reviews-slaider-mobile').owlCarousel({
+                dots: false,
+                margin: 16,
+                autoWidth: true,
+                items: 0,
+            });
+        } else {
+            owlCarousel && owlCarousel.destroy();
+        }
+    }).trigger('resize');
+})
 
   
